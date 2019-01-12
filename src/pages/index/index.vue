f6d040fcc72e88785fb9784032206da423a2f150
the third eye <623893018@qq.com>
2019-01-12 19:26:33 +0800

    note page

---
+++ src/pages/index/index.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+        <img class="bg" src="/static/images/plants.jpg" mode="widthFix"/>
+        <img class="wood1 wood" src="/static/images/wood-s.png" mode="widthFix" @click="toNote">
+        <img class="wood2 wood" src="/static/images/wood-s.png" mode="widthFix" @click="toCheck">
+        <div class="notes text" @click="toNote">记账</div>
+        <div class="check text" @click="toCheck">查看</div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    methods: {
+        toNote() {
+            wx.navigateTo({
+                url: '/pages/note/main'
+            })
+        },
+        toCheck() {
+            wx.navigateTo({
+                url: '/pages/check/main'
+            })
+        }
+    }
+}
+</script>
+
+
+<style>
+.bg {
+    width: 100%;
+    position: fixed;
+    z-index: -1;
+}
+
+.wood {
+    width: 200rpx;
+    margin: 100rpx;
+    border-radius: 10rpx;
+    box-shadow: 5rpx 10rpx 5rpx rgba(0, 0, 0, 0.8);
+}
+.wood1 {
+    margin: 100rpx;
+}
+.wood2 {
+    margin: 20rpx 100rpx;
+}
+
+.text {
+    position: absolute;
+    color: #201715;
+    text-shadow:0 0 2rpx #5f4139;
+    font-weight: 900;
+    font-size: 48rpx;
+    letter-spacing: 20rpx;
+}
+.notes {
+    top: 130rpx;
+    left: 140rpx;
+}
+.check {
+    left: 140rpx;
+    top: 382rpx;
+}
+
+</style>
+
+
---
+++ src/pages/note/note.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="container">
+    <span class="in-box box" :class="{active: showingBox == 1}" @click="showingBox = 1">钱出</span>
+    <span class="out-box box" :class="{active: showingBox == 2}" @click="showingBox = 2">钱进</span>
+    <div class="in-items items-box" v-if="showingBox == 1">
+      <p v-for="(item, index) in inItems" :key="item" @click="getItem(index)">{{item}}</p>
+    </div>
+    <div class="out-items items-box" v-if="showingBox == 2">
+      <p v-for="(item, index) in outItems" :key="item" @click="getItem(index)">{{item}}</p>
+    </div>
+    <form class="from" @submit="onSubmit">
+      <input class="details form-item" placeholder-class type="text" name="details" auto-focus="true" placeholder="账务备注">
+      <div class="price-date">
+        <input class="price form-item" placeholder-class type="digit" name="price" placeholder="￥0.00">
+        <picker class="picker form-item" mode="date" @change="onDate">
+          <view class="date">日期: {{date}}</view>
+        </picker>
+      </div>
+      <button form-type="submit">提交</button>
+    </form>
+  </div>
+</template>
+
+
+<script>
+import store from '../../store/store';
+
+export default {
+  data: {
+    inItems: [
+      "喂肚子",
+      "交通",
+      "旅游",
+      "通讯费",
+      "房租",
+      "水电",
+      "穿的",
+      "穿戴",
+      "洗护化妆",
+      "电子设备",
+      "医疗",
+      "保险",
+      "理财",
+      "还贷款",
+      "借朋友",
+      "还账",
+      "请客",
+      "打给父母",
+      "孩子花销",
+      "其它杂项"
+    ],
+    outItems: [
+      "工资",
+      "生意收入",
+      "理财收入",
+      "外快",
+      "奖金",
+      "加班费",
+      "报销",
+      "借钱",
+      "零花钱",
+      "其它进账"
+    ],
+    showingBox: 1,
+    item: '',
+    date: "0000-00-00",
+    warn: ''
+  },
+
+  methods: {
+    getItem(index) {
+      console.log(index);
+      this.item = this.inItems[index];
+      console.log(this.item);
+    },
+
+    onDate(e) {
+      console.log(e);
+      this.date = e.mp.detail.value;
+    },
+
+    onSubmit(e) {
+      console.log(e);
+      let data = e.mp.value;
+      if (this.item.length == 0) {
+          this.warn = '请选择分类';
+          wx.showToast({
+              title: '00'
+          })
+      } else if (date.details.length == 0) {
+          this.warn = '账务备注不能为空';
+      } else if (data.price == 0) {
+          this.warn = '金额不能为空';
+      } else if (this.date == '0000-00-00') {
+          this.warn = '请选择日期';
+      } else {
+          const db = wx.cloud.database();
+          db.collection('moneyInOut').add({
+            data: {
+                itemName: this.item, 
+                details: e.mp.detail.detils, 
+                price: e.mp.detail.value.price, 
+                date: this.date
+            }
+          })
+      }
+    }
+  }
+};
+</script>
+
+
+<style>
+.box {
+  display: inline-block;
+  width: 335rpx;
+  text-align: center;
+  background-color: #ffd7a6;
+  margin: 20rpx;
+  padding: 10rpx 0;
+  color: #201715;
+  text-shadow: 0 0 2rpx #5f4139;
+  letter-spacing: 40rpx;
+}
+.active {
+    background-color: #daa677;
+}
+
+.items-box p {
+  display: inline-block;
+  border: 1rpx solid #daa677;
+  border-radius: 10rpx;
+  color: #daa677;
+  background-color: #e1ffff;
+  margin: 20rpx;
+  font-size: 28rpx;
+  padding: 5rpx 10rpx;
+}
+.items-box p:hover {
+  background-color: #daa677;
+  color: #201715;
+}
+.input-placeholder {
+  color: #ffd7a6;
+  font-size: 24rpx;
+  letter-spacing: 5rpx;
+}
+form .form-item {
+  margin: 20rpx;
+  border: 1rpx solid #ffd7a6;
+  color: #201715;
+  font-size: 28rpx;
+  height: 56rpx;
+  line-height: 56rpx;
+  text-align: center;
+  background-color: #e1ffff;
+}
+.picker,
+.price {
+  width: 330rpx;
+  display: inline-block;
+}
+
+.price-date {
+    display: flex;
+}
+button {
+    background-color: #ffd7a6;
+    color: #fff;
+    letter-spacing: 40rpx;
+    margin-top: 100rpx;
+}
+</style>
+
